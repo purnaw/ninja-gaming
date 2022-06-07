4953489401bf0a48275c64144b5644c3cb0347c9
purnaw <45538530+purnaw@users.noreply.github.com>
2022-06-07 17:21:20 +0700

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>About Us</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum enim
+    doloribus itaque sapiente labore nesciunt deleniti, similique ratione eos
+    blanditiis neque hic tempore, architecto distinctio vero est adipisci
+    doloremque debitis!
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Ninja Gaming Guide</h2>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint sed,
+    molestiae ratione facere accusamus accusantium? Optio, nemo. Labore commodi
+    tempora minima odio molestias in voluptate illum molestiae. Nemo, possimus
+    ab.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
